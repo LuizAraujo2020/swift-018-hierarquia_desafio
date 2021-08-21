c1814b3ccf004d361283438930f630ab114a682b
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-21 20:55:40 -0300

    Initial setups

---
+++ swift-018-hierarquia_desafio/View/ButtonViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ButtonViewController.swift
+//  swift-018-hierarquia_desafio
+//
+//  Created by Luiz Carlos da Silva Araujo on 21/08/21.
+//
+
+import UIKit
+
+class ButtonViewController: UIButton {
+
+    // setar o titulo
+    //    setar o subtítulo
+    //    setar o max e min dos sliders
+    
+    //setar as cores
+    
+    //    setar a nova view
+    
+    //    chamar a nova tela
+    
+}
---
+++ swift-018-hierarquia_desafio/View/LabelViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LabelViewController.swift
+//  swift-018-hierarquia_desafio
+//
+//  Created by Luiz Carlos da Silva Araujo on 21/08/21.
+//
+
+import UIKit
+
+class LabelViewController: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
